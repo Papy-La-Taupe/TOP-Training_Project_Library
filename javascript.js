44b44c25199f3fea59f79b445af33c5431acc93c
Papy-La-Taupe <c.combes.pro@gmail.com>
2023-08-25 15:58:08 +0200

    add button + with some styling, and polish the addBook function

--- javascript.js
+++ javascript.js
@@ -6,39 +6,49 @@ function Book(title, author, pages, read){
     this.pages = pages
     this.read = read
   }
-
-function addBookToLibrary(title, author, pages, read) {
-  const newBook = new Book(title, author, pages, read);
-
-  const bookCard = document.createElement("div");
-  bookCard.classList.add("bookCard");
-
-  const bookTitle = document.createElement("p");
-  bookTitle.classList.add("bookTitle");
-  bookTitle.textContent = `- ${newBook.title} -`;
-
-  const bookAuthor = document.createElement("p");
-  bookAuthor.classList.add("bookAuthor");
-  bookAuthor.textContent = `Written by ${newBook.author}`;
-
-  const bookPages = document.createElement("p");
-  bookPages.classList.add("bookPages");
-  bookPages.textContent = `${newBook.pages} Pages`
-
-  const bookRead = document.createElement("p");
-  bookRead.classList.add("bookRead");
-  bookRead.textContent = newBook.read;
-
-  bookCard.appendChild(bookTitle);
-  bookCard.appendChild(bookAuthor);
-  bookCard.appendChild(bookPages);
-  bookCard.appendChild(bookRead);
-
-  const displayGrid = document.getElementById("DisplayGrid");
-  displayGrid.appendChild(bookCard);
-
-  myLibrary.push(newBook);
+ 
+function updateDisplayedCards(){
+    const displayGrid = document.getElementById('DisplayGrid');
+    displayGrid.innerHTML = '';
 }
 
+function newCardsDisplay(){
+    updateDisplayedCards();
+    for (let i = 0; i < myLibrary.length; i++) {
+        const bookCard = document.createElement("div");
+        bookCard.classList.add("bookCard");
 
-  
+        const bookTitle = document.createElement("p");
+        bookTitle.classList.add("bookTitle");
+        bookTitle.textContent = `- ${myLibrary[i].title} -`;
+
+        const bookAuthor = document.createElement("p");
+        bookAuthor.classList.add("bookAuthor");
+        bookAuthor.textContent = `Written by ${myLibrary[i].author}`;
+
+        const bookPages = document.createElement("p");
+        bookPages.classList.add("bookPages");
+        bookPages.textContent = `${myLibrary[i].pages} Pages`
+
+        const bookRead = document.createElement("p");
+        bookRead.classList.add("bookRead");
+        bookRead.textContent = myLibrary[i].read;
+
+        bookCard.appendChild(bookTitle);
+        bookCard.appendChild(bookAuthor);
+        bookCard.appendChild(bookPages);
+        bookCard.appendChild(bookRead);
+
+        const displayGrid = document.getElementById("DisplayGrid");
+        displayGrid.appendChild(bookCard);
+    }
+}
+
+function addBookToLibrary(title, author, pages, read) {
+    const newBook = new Book(title, author, pages, read);
+    myLibrary.push(newBook);
+    newCardsDisplay();    
+}
+
+addBookToLibrary("testbook1", "joker", "23", "read");
+addBookToLibrary("testbook2", "jokster", "85", "not read");
